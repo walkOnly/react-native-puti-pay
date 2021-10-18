13f9c40c72d56fd80ed50c10f688b46858875d4a
tiangui <tiangui@huanqiu.com>
2021-10-18 14:14:10 +0800

    android.support -> androidx

--- android/src/main/java/com/puti/paylib/XWXPayEntryActivity.java
+++ android/src/main/java/com/puti/paylib/XWXPayEntryActivity.java
@@ -3,7 +3,7 @@ package com.puti.paylib;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
